dda6f44224bc0f087ec37115317cdd46dcc8d8fe
Mark Newman <mark@trinetteandmark.com>
2020-05-01 23:47:17 -0400

    doc to corpus

--- documents-to-corpus/README.md
+++ documents-to-corpus/README.md
@@ -1,12 +1,15 @@
 # Documents to Corpus
 
 A collection of documents is a corpus in name only.
-Unstructured data is just like its continuous cousin
-in that it needs cleaned and prepared before it becomes useful.
-This script should serve as the baseline for what can be done.
+Unstructured data is just like its continuous cousin in that it needs cleaned and prepared before it becomes useful.
+
 The difference in this script and others in this collection is that each corpus will have its own nuanced data cleaning process.
+As such, this script should serve _only_ as a baseline for what _can_ be done.
+In some cases this is a simple find/replace on all files before running the code as-is.
+Other times extensive evaluation is needed.
 As an example, academic works have a whole different set of abbreviations when compared to general English.
 These differences produce a bad tokenization when not accounted for in the process.
+In the case where extensive evaluation is undertaken, it is appropriate to copy/paste this code to the new project and make modifications there.
 
 ## Run the script
 
--- documents-to-corpus/process.py
+++ documents-to-corpus/process.py
@@ -1,104 +1,67 @@
 from argparse import ArgumentParser
 from collections import namedtuple
-from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktParameters
-from shutil import rmtree
-import csv
-import json
+from nltk.tokenize.punkt import PunktSentenceTokenizer
+from nltk.tokenize.treebank import TreebankWordTokenizer
 import pathlib
 import progressbar as pb
 
-punkt_param = PunktParameters()
-punkt_param.abbrev_types = set(['al', 'fig', 'i.e', 'e.g'])
-sent_tokenize = PunktSentenceTokenizer(punkt_param)
+sent_tokenize = PunktSentenceTokenizer()
+word_tokenize = TreebankWordTokenizer()
 
-cite_span = namedtuple('cite_span', 'start end')
-article = namedtuple('article', 'id abstract body')
-
-def convert_folder_to_corpus(folder_in, folder_out):
-    folder_in = pathlib.Path(folder_in)
-    folder_out = pathlib.Path(folder_out)
-    create_folder_structure(folder_out)
-    errors = []
+# Iterates over all the documents in a corpus creating a new collection of sentence tokenized documents
+def documents_to_corpus(path_in: pathlib.Path, path_out: pathlib.Path) -> None:
+    __is_folder_readable(path_in)
+    __is_folder_writable(path_out)
     i = 1
     widgets = [ 'Converting File # ', pb.Counter(), ' ', pb.Timer(), ' ', pb.BouncingBar(marker = '.', left = '[', right = ']')]
     with pb.ProgressBar(widgets = widgets) as bar:
-        for file_name in folder_in.iterdir():
-            if file_name.is_file() and file_name.suffix.lower() == '.json':
+        for file_name in path_in.iterdir():
+            if file_name.is_file() and file_name.suffix.lower() == '.txt':
                 bar.update(i)
                 i = i + 1
-                article = parse_json_to_article(file_name)
-                if article == None:
-                    errors.append(file_name.stem)
-                else:
-                    write_article(folder_out, article)
-    write_errors(folder_out, errors)
+                sentences = __tokenize_document(file_name)
+                file_out = path_out.joinpath(f'{file_name.stem}.txt')
+                with file_out.open('w', encoding = 'utf-8') as file_out:
+                    for sentence in sentences:
+                        file_out.write(f'{sentence}\n')
 
-def create_folder_structure(folder_out):
-    if folder_out.exists():
-        if folder_out.is_dir():
-            rmtree(folder_out)
-        else:
-            folder_out.unlink()
-    folder_out.mkdir(parents = True)
-    folder_out.joinpath('./abstract').mkdir()
-    folder_out.joinpath('./body').mkdir()
+# makes sure our parameters are good
+def __is_folder_readable(folder: pathlib.Path) -> None:
+    if not folder.exists():
+        raise FileNotFoundError(str(folder))
+    elif not folder.is_dir():
+        raise NotADirectoryError(str(folder))
+def __is_folder_writable(folder: pathlib.Path) -> None:
+    if not folder.exists():
+        folder.mkdir(parents = True)
+    elif not folder.is_dir():
+        raise NotADirectoryError(str(folder))
 
-def parse_json_to_article(file_name):
-    file_in = file_name
-    with file_in.open('r', encoding = 'utf-8') as file_in:
-        raw_data = json.load(file_in)
-    id = raw_data['paper_id']
-    abstract = extract_paragraphs(raw_data['abstract'])
-    abstract = convert_to_sentences(abstract)
-    body = extract_paragraphs(raw_data['body_text'])
-    body = convert_to_sentences(body)
-    return article(id, abstract, body) if len(abstract) > 0 and len(body) > 0 else None
-
-def extract_paragraphs(paragraphs):
-    result = [clean_paragraph(paragraph) for paragraph in paragraphs]
+# Run Punkt then PENN Treebank to clean up sentences
+def __tokenize_document(document_name: pathlib.Path) -> list:
+    with document_name.open('r') as document:
+        lines = document.readlines()
+    result = []
+    for line in lines:
+        sentences = sent_tokenize.tokenize(line.strip())
+        for i in range(0, len(sentences)):
+            sentence = sentences[i]
+            words = word_tokenize.tokenize(sentence)
+            sentences[i] = ' '.join(words)
+        result.extend(sentences)
     return result
-
-def clean_paragraph(paragraph):
-    text = paragraph['text']
-    cite_spans = paragraph['cite_spans']
-    cite_spans = [cite_span(x['start'], x['end']) for x in cite_spans]
-    for x in sorted(cite_spans, key = lambda x: x.end, reverse = True):
-        text = text[0: x.start:] + text[x.end + 1::]
-    return text
-
-def convert_to_sentences(paragraphs):
-    result = [sent_tokenize.tokenize(paragraph) for paragraph in paragraphs]
-    return result
-
-def write_article(folder_out, article):
-    write_article_part(folder_out.joinpath(f'./abstract/{article.id}.txt'), article.abstract)
-    write_article_part(folder_out.joinpath(f'./body/{article.id}.txt'), article.body)
-
-def write_article_part(file_out, paragraphs):
-    first = True
-    with file_out.open('w', encoding = 'utf-8') as file_out:
-        for paragraph in paragraphs:
-            if first:
-                first = False
-            else:
-                file_out.write('\n')
-            for sentence in paragraph:
-                file_out.write(sentence)
-                file_out.write('\n')
-
-def write_errors(folder_out, errors):
-    errors_file = folder_out.joinpath('./error.csv')
-    with errors_file.open('w', encoding = 'utf-8', newline='') as errors_file:
-        writer = csv.writer(errors_file, delimiter = ',', quotechar = '"', quoting = csv.QUOTE_ALL)
-        writer.writerow(['incomplete article'])
-        for error in errors:
-            writer.writerow([error])
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('-in', '--folder-in', help = 'Folder containing the raw JSON files', required = True)
-    parser.add_argument('-out', '--folder-out', help = 'Folder containing the newly created text corpus', required = True)
+    parser.add_argument(
+        '-in', '--folder-in',
+        help = 'Folder containing the source documents',
+        default = 'd:/corpus_in')
+    parser.add_argument(
+        '-out', '--folder-out',
+        help = 'Folder containing the sentence tokenized documents',
+        default = 'd:/corpus_out')
     args = parser.parse_args()
     print(f'folder in: {args.folder_in}')
     print(f'folder out: {args.folder_out}')
-    convert_folder_to_corpus(args.folder_in, args.folder_out)
+    documents_to_corpus(pathlib.Path(args.folder_in), pathlib.Path(args.folder_out))
