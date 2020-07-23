0d62136198ad7b761a8bba710f38b11ee707d5b3
Mark Newman <mark@trinetteandmark.com>
2020-07-23 09:54:51 -0400

    remove stopwords

---
+++ code/remove-stopwords-from-corpus.py
@@ -0,0 +1,57 @@
+import nltk
+import pathlib
+import shutil
+import uuid
+import progressbar as pb
+import utils as u
+from argparse import ArgumentParser
+from typeguard import typechecked
+
+# Iterates over all the documents in a corpus, removing all the NLTK stopwords
+@typechecked
+def remove_stopwords_from_corpus(path_in: pathlib.Path, path_out: pathlib.Path) -> None:
+    u.assert_folder_is_readable(path_in)
+    u.assert_folder_is_writable(path_out)
+    i = 1
+    stopwords = nltk.corpus.stopwords.words('english')
+    stopwords = set(stopwords)
+    widgets = [ 'Removing Stopwords from Corpus # ', pb.Counter(), ' ', pb.Timer(), ' ', pb.BouncingBar(marker = '.', left = '[', right = ']')]
+    with pb.ProgressBar(widgets = widgets) as bar:
+        for file_name in path_in.iterdir():
+            if u.is_corpus_document(file_name):
+                bar.update(i)
+                i = i + 1
+                sentences = __remove_stopwords_from_document(file_name, stopwords)
+                file_out = path_out.joinpath(f'{file_name.stem}.txt')
+                with file_out.open('w', encoding = 'utf-8') as file_out:
+                    for sentence in sentences:
+                        file_out.write(f'{sentence}\n')
+
+# removes the stopwords from a single document
+@typechecked
+def __remove_stopwords_from_document(document_name: pathlib.Path, stopwords: set) -> list:
+    with document_name.open('r', encoding = 'utf-8') as document:
+        lines = document.readlines()
+    result = []
+    for line in lines:
+        line = line.strip()
+        words = [token for token in line.split(' ')]
+        words = [word for word in words if word not in stopwords]
+        sentence = ' '.join(words)
+        result.append(sentence)
+    return result
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument(
+        '-in', '--folder-in',
+        help = 'Folder containing the source corpus',
+        default = 'd:/corpus_in')
+    parser.add_argument(
+        '-out', '--folder-out',
+        help = 'Folder containing the corpus free of stopwords',
+        default = 'd:/corpus_out')
+    args = parser.parse_args()
+    print(f'folder in: {args.folder_in}')
+    print(f'folder out: {args.folder_out}')
+    remove_stopwords_from_corpus(pathlib.Path(args.folder_in), pathlib.Path(args.folder_out))
---
+++ docs/remove-stopwords-from-corpus.md
@@ -0,0 +1,22 @@
+# Corpus to Reading Level
+
+Transforms all the documents in the corpus by removing all the 'English' stopwords.
+The list used is NLTK's default.
+
+## Run the script
+
+You may need to get the stopwords as NLTK may not download it automatically.
+
+```{shell}
+python.exe -c "import nltk;nltk.download('stopwords')"
+```
+
+The script is run in accordance with the [general form](../README.md#scripts)
+
+## Academic Boilerplate
+
+Below is the suggested text to add to the Methods and Materials section of your paper when using this building block.
+The references can be found [here](./references.bib)
+
+> This tool has not been accepted into a published paper, hence it has no boilerplate.
+> Yet.
