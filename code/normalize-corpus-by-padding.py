0473265779aef97d15fc814157b3c8a1841f7dff
Mark Newman <mark@trinetteandmark.com>
2020-07-29 17:19:41 -0400

    padding

---
+++ code/normalize-corpus-by-padding.py
@@ -0,0 +1,58 @@
+import csv
+import pathlib
+import progressbar as pb
+import utils as u
+from argparse import ArgumentParser
+from typeguard import typechecked
+
+# Iterates over all the documents in a corpus, normalizing them by applying `__remove_numbers_from_document()`
+@typechecked
+def normalize_corpus_by_padding(path_in: pathlib.Path, path_out: pathlib.Path, max_length: int) -> None:
+    u.assert_folder_is_readable(path_in)
+    u.assert_folder_is_writable(path_out)
+    i = 1
+    widgets = [ 'Normalize Document # ', pb.Counter(), ' ', pb.Timer(), ' ', pb.BouncingBar(marker = '.', left = '[', right = ']')]
+    with pb.ProgressBar(widgets = widgets) as bar:
+        for file_name in path_in.iterdir():
+            if u.is_corpus_document(file_name):
+                bar.update(i)
+                i = i + 1
+                sentences = __normalize_document_by_padding(file_name, max_length)
+                u.write_document(path_out, file_name, sentences)
+
+@typechecked
+def __normalize_document_by_padding(document_name: pathlib.Path, max_length: int) -> list:    
+    lines = u.read_document(document_name)
+    results = []
+    for line in lines:
+        tokens = line.split()
+        if len(tokens) > max_length:
+            tokens = tokens[0:max_length]
+        max_length = max_length - len(tokens)
+        if len(tokens) > 0:
+            results.append(' '.join(tokens))
+    if max_length > 0:
+        tokens = ['0' for _ in range(0, max_length)]
+        results.append(' '.join(tokens))
+    return results    
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument(
+        '-in', '--folder-in',
+        help = 'Folder containing the source documents',
+        default = 'd:/corpus_in')
+    parser.add_argument(
+        '-out', '--folder-out',
+        help = 'Folder containing the vectorise documents',
+        default = 'd:/corpus_out')
+    parser.add_argument(
+        '-l', '--max-length',
+        help = 'Max length the document can take on',
+        type = int,
+        default = '1000')
+    args = parser.parse_args()
+    print(f'folder in: {args.folder_in}')
+    print(f'folder out: {args.folder_out}')
+    print(f'max length: {args.max_length}')
+    normalize_corpus_by_padding(pathlib.Path(args.folder_in), pathlib.Path(args.folder_out), int(args.max_length))
---
+++ docs/normalize-corpus-by-padding.md
@@ -0,0 +1,26 @@
+# Normalize Corpus by Padding
+
+Deep Learning networks typically need a fixed shape for their inputs.
+This script truncates all documents above a given length and right pads documents below that length. 
+This script is intended to be run _after_ [vectorization](./vectorize-corpus.md)
+
+## Run the script
+
+These scripts need to be run in a modified manner compared to the [general form](../README.md#scripts).
+There is another parameter called `length` that is needed to provide a consistent input shape.
+Please update step 4 in the [general form](../README.md#scripts) to be:
+
+* Run `python normalize-corpus-by-padding.py -in d:/corpus_in -out d:/corpus_out -l 2000`.
+
+## Academic Boilerplate
+
+Below is the suggested text to add to the Methods and Materials section of your paper when using this building block.
+The references can be found [here](./references.bib)
+
+> Each document was normalized to provide a consistent input shape to the deep learning network.
+> The below normalization steps were undertaken:
+>
+> 1. Documents above length `xxx` were truncated.
+> 2. Documents below length `xxx` were right padded with zeros.
+>
+> The normalization script can be found in the companion repository ^[http://www.github.com/{user}/{repo}].
